66053dced3878d409aef1577f63b6dc7a5bf0980
web <web@training.workrocks.ua>
2022-02-10 21:01:48 +0200

    fixed bages

--- routes/Searches.ts
+++ routes/Searches.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getSearches, getSearchMiddleWare, createNewSearch, getSearch} from "../controllers/Searches";
+import {getSearches, getSearchMiddleWare, createNewSearch, getSearch, updateSearch} from "../controllers/Searches";
 const router = express.Router();
 import coordinatorsRouter from './Coordinators'
 import eventRouter from './Event'
@@ -8,11 +8,11 @@ import participantsRouter from './Participants'
 router.get('/',getSearches);
 router.post('/', createNewSearch);
 router.get('/:id', getSearch);
+router.put('/:id', getSearchMiddleWare, updateSearch)
 //api/v1/searches/1/coordinators
 router.use('/:id/coordinators', getSearchMiddleWare, coordinatorsRouter);
 router.use('/:id/events', getSearchMiddleWare, eventRouter);
 router.use('/:id/participants', getSearchMiddleWare, participantsRouter);
 //router.use('/:id/coordinates', getSearchMiddleWare, coordinatesRouter)
-
 
 export default router;
