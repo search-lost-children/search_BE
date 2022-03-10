08c9c1002ea029766e85201d1c0f86e69a50455d
Kostiantyn Huk <khuk@mural.co>
2022-03-10 10:12:50 +0200

    addes task request handler

--- controllers/tasks.ts
+++ controllers/tasks.ts
@@ -1,11 +1,13 @@
-import {NextFunction, Response, Request} from "express";
+import {NextFunction, Request, Response} from "express";
 import schema from "../validationSchemas/newTask"
 import {getConnection} from "typeorm";
 import Task from "../src/entity/Task";
 import Search from "../src/entity/Search";
 import TaskTypes from "../src/enums/taskTypes.enum";
-// require('express-async-errors');
-
+import Roles from "../src/enums/roles.enum";
+import User from "../src/entity/Users";
+import Participant from "../src/entity/Participant";
+import Squad from "../src/entity/Squads";
 
 export async function createNewTask(req: Request, res: Response, next: NextFunction) {
     const result = schema.validate(req.body);
@@ -32,10 +34,41 @@ export async function createNewTask(req: Request, res: Response, next: NextFunct
     res.send (task)
 }
 
-export async function getSearchTasks (req: Request, res: Response, next: NextFunction){
+export async function getSearchTasks (req: Request, res: Response, next: NextFunction) {
+    const squadsRepo = getConnection().getRepository(Squad);
+    const participantRepo = getConnection().getRepository(Participant);
     const repository = getConnection().getRepository(Task);
-    const tableInfo = await repository.find({ where: { search: req.search} });
-    res.send(tableInfo)
+    const user = req.user as User;
+    const search = req.search as Search;
+    let options;
+    if (user.role === Roles.admin) {
+        options = { where: { search } };
+        const tasks = await repository.find(options);
+        res.send(tasks)
+    } else {
+        // const [squad, participant] = await Promise.all([
+        //     squadsRepo.find({where: {se}})
+        // ])
+        const participant = await participantRepo.findOne({where: {search, user}, relations:['squad', 'squad.tasks','tasks']})
+        if(!participant) {
+            res.status(400)
+            return res.send('participant not found')
+        }
+        let tasksFromParticipant: Task[] = [];
+        let tasksFromSquad: Task[] = [];
+        if (participant.tasks) {
+            tasksFromParticipant = participant.tasks
+        }
+        if (participant?.squad?.tasks) {
+            tasksFromSquad = participant.squad.tasks
+        }
+        const tasks = [
+            ...tasksFromParticipant,
+            ...tasksFromSquad
+        ]
+        res.send(tasks)
+    }
+
 }
 
 
